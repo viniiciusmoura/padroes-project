6b3127961b150865f5a39ae6838c781b824728c4
viniiciusmoura <viniciusmirandamoura5@gmail.com>
2023-11-19 09:12:57 -0300

    implementação funcional Brasdesco

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import org.example.entity.LeituraRetorno;
-import org.example.entity.LeituraRetornoBancoBrasil;
-import org.example.entity.LeituraRetornoBradesco;
 import org.example.entity.ProcessarBoletos;
 
 import java.net.URI;
@@ -10,21 +7,16 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
+        String caminho = "file:///home/vinicius/Documentos/IFTO/PP/stratgy/padroes-project/banco-brasil-1.csv";
+        String caminhoBra ="file:///home/vinicius/Documentos/IFTO/PP/stratgy/padroes-project/bradesco-1.csv";
 
-        System.out.println("Hello world!");
-        var leituraRetorno = new LeituraRetornoBancoBrasil();
-        var processador = new ProcessarBoletos(leituraRetorno);
-        String caminho = "file:///home/vinicius/Documentos/IFTO/6_SEMESTRE/PP/Aula02/getBoleto/banco-brasil-1.csv";
-        String caminhoBra ="file:///home/vinicius/Documentos/IFTO/6_SEMESTRE/PP/Aula02/getBoleto/bradesco-1.csv";
+        System.out.println("----Banco do Brasil----");
+        ProcessarBoletos processador = new ProcessarBoletos(ProcessarBoletos::lerBancoBrasil);
         processador.processar(new URI(caminho));
-        configurarLeituraRetorno(leituraRetorno);
-        //Bradesco
-        var leituraRetornoBra = new LeituraRetornoBradesco();
-        var processadorBradesco = new ProcessarBoletos(leituraRetornoBra);
-        processadorBradesco.processar(new URI(caminhoBra));
-    }
 
-    private static void configurarLeituraRetorno(LeituraRetornoBancoBrasil leituraRetorno) {
+        System.out.println("\n----Banco do Bradesco----");
+        ProcessarBoletos processadorBras = new ProcessarBoletos(ProcessarBoletos::lerBradesco);
+        processadorBras.processar(new URI(caminhoBra));
 
     }
 }
--- src/main/java/org/example/entity/ProcessarBoletos.java
+++ src/main/java/org/example/entity/ProcessarBoletos.java
@@ -1,20 +1,92 @@
 package org.example.entity;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 
 public class ProcessarBoletos {
-    private LeituraRetorno leituraRetorno;
 
+    private static DateTimeFormatter FORMT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public ProcessarBoletos(LeituraRetorno leituraRetorno) {
-        this.leituraRetorno = leituraRetorno;
+    private static DateTimeFormatter FORMT_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    private Function<URI, List<Boleto>> leituraRetornoN;
+
+    public ProcessarBoletos(Function<URI, List<Boleto>> leituraRetornoN)
+    {
+        this.leituraRetornoN = leituraRetornoN;
     }
 
-    public void processar(URI caminhoArquivo){
-        leituraRetorno.lerArquivo(caminhoArquivo);
-    };
+    public void processar(URI file)
+    {
+        List<Boleto> boletos = leituraRetornoN.apply(file);
+        for (Boleto boleto : boletos)
+        {
+            System.out.println(boleto);
+        }
+    }
 
-    public void setLeituraRetorno(LeituraRetorno leituraRetorno) {
-        this.leituraRetorno = leituraRetorno;
+    public static List<Boleto> lerBancoBrasil(URI file)
+    {
+        try {
+            var listLine = Files.readAllLines(Paths.get(file));
+            List<Boleto> boletos = new ArrayList<>();
+
+            for (var linha : listLine) {
+                var vetor = linha.split(";");
+                var boleto = new Boleto();
+                boleto.setId(Integer.parseInt(vetor[0]));
+                boleto.setCodBanco(vetor[1]);
+
+                boleto.setDataVencimento(LocalDate.parse(vetor[2], FORMT));
+                boleto.setDataEmissao(LocalDate.parse(vetor[3], FORMT).atTime(0,0));
+
+                boleto.setCpfCliente(vetor[4]);
+                boleto.setValor(Double.parseDouble(vetor[5]));
+                boleto.setMulta(Double.parseDouble(vetor[6]));
+                boleto.setAgencia(vetor[7]);
+                boletos.add(boleto);
+            }
+            return boletos;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static List<Boleto> lerBradesco(URI file)
+    {
+        try {
+            var listLine = Files.readAllLines(Paths.get(file));
+            List<Boleto> boletos = new ArrayList<>();
+
+            for (var linha:listLine){
+                var vetor = linha.split(";");
+                var boleto = new Boleto();
+                boleto.setId(Integer.parseInt(vetor[0]));
+                boleto.setCodBanco(vetor[1]);
+                boleto.setAgencia(vetor[2]);
+                boleto.setContaBancaria(vetor[3]);
+                boleto.setDataVencimento(LocalDate.parse(vetor[4], FORMT));
+                boleto.setDataEmissao(LocalDateTime.parse(vetor[5], FORMT_DATA));
+                boleto.setCpfCliente(vetor[6]);
+                boleto.setValor(Double.parseDouble(vetor[7]));
+                boleto.setMulta(Double.parseDouble(vetor[8]));
+                boleto.setJuros(Double.parseDouble(vetor[9]));
+                boletos.add(boleto);
+            }
+
+            return boletos;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
